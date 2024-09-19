9718930f70b4acf5e73fcbefde124dd21d49fc1d
Yousre Ramadan <https://github.com/Yousre20/Yousre20>
2024-09-19 13:31:32 +0300

    Project

---
+++ home.js
@@ -0,0 +1,9 @@
+// Add to Cart Button Functionality
+const cartButtons = document.querySelectorAll('.add-to-cart');
+
+cartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        // Placeholder for adding the product to the cart
+        alert('Item added to your cart!');
+    });
+});
